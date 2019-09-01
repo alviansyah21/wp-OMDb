2765fd33944b034133a7f1fbf729bee1ca782764
Muhammad Alviansyah <54306872+alviansyah21@users.noreply.github.com>
2019-09-01 23:52:18 +0700

    Update head.php

--- _partials/head.php
+++ _partials/head.php
@@ -2,15 +2,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     
-    <script type="text/javascript" src="assets/jquery/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="assets/bootstrap/js/popper.min.js"></script>
-    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <script type="text/javascript" src="assets/angular/angular.min.js"></script>
-    <script type="text/javascript" src="assets/github/buttons.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
+    <script type="text/javascript" src="https://buttons.github.io/buttons.js"></script>
 
     <script type="text/javascript" src="assets/js/OMDbAPISearch.js"></script>
 
